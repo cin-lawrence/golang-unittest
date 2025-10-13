f0f3f96e852cf35741778e68dd12fcb35eb48f18
Lawrence Feng <spnether1411@gmail.com>
2025-10-13 14:49:30 +0700

    feat: add chap 13

---
+++ ch13_sync/go.mod
@@ -0,0 +1,3 @@
+module golang-unittest/ch13
+
+go 1.24.4
---
+++ ch13_sync/sync.go
@@ -0,0 +1,27 @@
+package main
+
+import "sync"
+
+type ICounter interface {
+	Inc()
+	Value() int
+}
+
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
