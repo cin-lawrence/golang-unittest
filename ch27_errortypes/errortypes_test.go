48683be2beffab1321e81db24e52a99080e8dbd7
Lawrence Feng <spnether1411@gmail.com>
2025-11-03 11:45:34 +0700

    feat: add chap 27

---
+++ ch27_errortypes/errortypes_test.go
@@ -0,0 +1,62 @@
+package errortypes
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type BadStatusError struct {
+	URL    string
+	Status int
+}
+
+func (b BadStatusError) Error() string {
+	return fmt.Sprintf("did not get 200 from %s, got %d", b.URL, b.Status)
+}
+
+func DumbGetter(url string) (string, error) {
+	res, err := http.Get(url)
+
+	if err != nil {
+		return "", fmt.Errorf("problem fetching from %s, %v", url, err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return "", BadStatusError{URL: url, Status: res.StatusCode}
+	}
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	return string(body), nil
+}
+
+func TestDumbGetter(t *testing.T) {
+	t.Run("when you don't get a 200 you get a status error", func(t *testing.T) {
+		svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, h *http.Request) {
+			w.WriteHeader(http.StatusTeapot)
+		}))
+		defer svr.Close()
+
+		_, err := DumbGetter(svr.URL)
+		if err == nil {
+			t.Fatal("expected an error")
+		}
+
+		var got BadStatusError
+		isBadStatusError := errors.As(err, &got)
+		want := BadStatusError{URL: svr.URL, Status: http.StatusTeapot}
+
+		if !isBadStatusError {
+			t.Fatalf("was not a BadStatusError, got %T", err)
+		}
+
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
